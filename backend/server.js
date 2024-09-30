9b3d35adae30f65e462fb9125dbc89779d6cdada
DDD5677 <djumaqulovdonisher@mail.ru>
2024-09-30 09:44:47 +0500

    change urls

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
    cors: {
-      origin: ["http://localhost:5173"],
+      origin: [
+         "http://localhost:5173",
+         "https://presentation-app-frontend.onrender.com/",
+      ],
    },
 });
 app.get("/", (req, res) => {
--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
-const URL =
-   process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+const URL = "https://presentation-app-dyvp.onrender.com/";
+//process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
 
 export const socket = io(URL);
 
